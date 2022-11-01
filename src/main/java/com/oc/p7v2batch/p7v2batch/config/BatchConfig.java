a4061e2e0aa858099c0cac73386a9e0157312eee
louis poirot <louispoirot38@gmail.com>
2022-11-01 11:48:37 +0100

    first commit

---
+++ src/main/java/com/oc/p7v2batch/p7v2batch/config/BatchConfig.java
@@ -0,0 +1,33 @@
+package com.oc.p7v2batch.p7v2batch.config;
+
+import com.oc.p7v2batch.p7v2batch.service.BatchService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+
+
+@Configuration
+@EnableBatchProcessing
+@EnableScheduling
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Log4j2
+public class BatchConfig {
+    private final BatchService batchService;
+
+    @Scheduled(cron=" * * * * * SUN") // (s min h j m jourSemaine)
+    public Job job() throws IOException {
+        log.info("in BatchConfig in job method");
+            log.info("in BatchConfig in job method when job is enable");
+            batchService.batchProcessing();
+        return null;
+
+    }
+}
